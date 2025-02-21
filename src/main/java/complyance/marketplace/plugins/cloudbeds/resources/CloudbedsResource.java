f8d78a6bf44c9ce55a23a69ebee1a1fbfda06814
Tanuj <tanuj@complyance.io>
2025-02-21 11:37:22 +0530

    Commenting out test cases and cloudbeds url update

--- src/main/java/complyance/marketplace/plugins/cloudbeds/resources/CloudbedsResource.java
+++ src/main/java/complyance/marketplace/plugins/cloudbeds/resources/CloudbedsResource.java
@@ -5,7 +5,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/plugins/cloudbeds")
+@Path("/cloudbeds")
 public class CloudbedsResource {
 
     @GET
